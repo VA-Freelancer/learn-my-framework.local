04f0afc21a2aa77c3af0a44db9180bd3a1682880
VA-Freelancer <vproger@vk.com>
2022-09-13 01:59:25 +0400

    Initial commit

---
+++ config/init.php
@@ -0,0 +1,18 @@
+<?php
+
+define("DEBUG", 1);
+define("ROOT", dirname(__DIR__));
+define("WWW", ROOT . '/public');
+define("APP", ROOT . '/app');
+define("CORE", ROOT . '/vendor/vproger');
+define("HELPERS", ROOT . '/vendor/vproger/helpers');
+define("CACHE", ROOT . '/tmp/cache');
+define("LOGS", ROOT . '/tmp/logs');
+define("CONFIG", ROOT . '/config');
+define("LAYOUT", "ishop");
+define("PATH", "http://my-framework.local");
+define("ADMIN", "http://my-framework.local/admin");
+define("NO_IMAGE", "uploads/no_image.jpg");
+
+
+require_once ROOT . "/vendor/autoload.php";
