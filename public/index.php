4da2ccfd348fb6a74b5fe12635f68dd6421d6df3
VA-Freelancer <vproger@vk.com>
2022-09-16 01:40:53 +0400

    добавлен класс ошибок, добавден страницы с ошибками, add патерн сингалтон

--- public/index.php
+++ public/index.php
@@ -5,8 +5,9 @@ if (PHP_MAJOR_VERSION < 8) {
 }
 
 require_once dirname(__DIR__) . '/config/init.php';
+require_once HELPERS. '/functions.php';
+require_once CONFIG. '/routes.php';
 
 new \vproger\App();
 
-throw new \Exception('Возникла ошибочка');
-//var_dump(\vproger\App::$app->getProperties());
+debug(\vproger\Router::getRoutes());
